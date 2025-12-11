9f40f10d7a0607c1c087d1ea051c6fb23aeb1706
DSAnup <anup12.m@gmail.com>
2025-12-11 18:49:14 +0600

    prefetch filtering

--- playground/views.py
+++ playground/views.py
@@ -4,17 +4,18 @@ from store.models import Product, Customer, Collection, Order, OrderItem
 
 # Create your views here.
 def home(request):
-    products = Product.objects.filter(inventory__lt=10)
+    products = Product.objects.prefetch_related('promotions').select_related('collection').all()
     customers = Customer.objects.filter(email__icontains='.com')
     collections = Collection.objects.filter(featured_product__isnull=True)
-    order = Order.objects.filter(customer__id=1)
-    order_items = OrderItem.objects.filter(product__collection__id=3)
+    order = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    # order_items = OrderItem.objects.all()
+
     context = {
         'products': products,
         'customers': customers,
         'collections': collections,
-        'order': order,
-        'order_items': order_items,
+        'orders': order,
+        # 'order_items': order_items,
         'message': 'Welcome to the Storefront Playground!'
     }
     return render(request, 'hello.html', context)
