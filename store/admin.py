4b369b22c6cb37a0c8ea4ac9c8b2329db1f0cd59
DSAnup <anup12.m@gmail.com>
2025-12-13 15:10:05 +0600

    custom filter & action

--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,31 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
 from .models import *
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory_status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('low', 'Low Inventory'),
+            ('sufficient', 'Sufficient Inventory'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'low':
+            return queryset.filter(inventory__lt=10)
+        if self.value() == 'sufficient':
+            return queryset.filter(inventory__gte=10)
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection']
 
@@ -19,13 +37,20 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory < 10:
             return 'Low'
         return 'OK'
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were successfully updated.'
+        )
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email', 'phone', 'membership', 'orders_count']
     list_editable = ['phone', 'membership']
-    search_fields = ['first_name__istartswith', 'last_name__istartswith']
     list_per_page = 10
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
     @admin.display(ordering='orders_count')
     def orders_count(self, customer):
         url = reverse('admin:store_order_changelist') + '?' + urlencode({'customer__id': str(customer.id)})
