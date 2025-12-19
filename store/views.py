b3b0cb8241a595b54132e4ae95d747280bc29f7c
DSAnup <anup12.m@gmail.com>
2025-12-19 12:17:02 +0600

    added main urls

--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def product_list(request):
+    return Response({"message": "List of products"})
+
+@api_view(['GET'])
+def product_detail(request, product_id):
+    return Response({"message": f"Details of product {product_id}"})
