f01476e207fefe4e32335ecad99921fe211ab3f9
DSAnup <anup12.m@gmail.com>
2025-12-30 17:02:50 +0600

    django filters package

--- store/views.py
+++ store/views.py
@@ -1,19 +1,20 @@
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
+
+from store.filters import ProductFilter
 from .models import OrderItem, Product, Collection, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.select_related('collection').all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
-    def get_queryset(self):
-        queryset = Product.objects.select_related('collection').all()
-        collection_id = self.request.query_params.get('collection_id')
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+
     def get_serializer_context(self):
         return {'request': self.request}
     
